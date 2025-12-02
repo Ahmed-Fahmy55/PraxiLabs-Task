c3d05ba8589af6369b47c7e151762e26845c8a92
Kizaro elgen <ahmed.fahmy@bltzo.com>
2025-12-02 14:52:14 +0200

    Enemy types

---
+++ Assets/_Project/_Scripts/Enemy/Enemy Types/ArcherEnemy.cs
@@ -0,0 +1,13 @@
+﻿using Praxi.Enemy.Base;
+using UnityEngine;
+
+namespace Praxi.Enemy
+{
+    public class ArcherEnemy : EnemyBase
+    {
+        public override void Attack(Transform target)
+        {
+            Debug.Log("Archer attacks the target!");
+        }
+    }
+}
---
+++ Assets/_Project/_Scripts/Enemy/Enemy Types/FighterEnemy.cs
@@ -0,0 +1,14 @@
+﻿using Praxi.Enemy.Base;
+using UnityEngine;
+
+namespace Praxi.Enemy
+{
+    public class FighterEnemy : EnemyBase
+    {
+
+        public override void Attack(Transform target)
+        {
+            Debug.Log("FighterEnemy attacks the target!");
+        }
+    }
+}
